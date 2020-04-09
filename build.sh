98ef8c86f9cb71349ca58da60423eacbf71ccbe2
Somayyah <55834191+Somayyah@users.noreply.github.com>
2020-04-09 12:07:09 +0300

    changed date

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ echo "Running the build script..."
 
 HOSTNAME=$(hostname)
 echo "I am ${HOSTNAME}."
-echo "The time is $(date %s)."
+echo "The time is $(date +%m-%d-%Y)."
 echo "The logged in user is: ${USER}."
 echo "Current working directory: ${PWD}."
