84cb58075ad6f69d010a7b22b9864c8736aa0deb
Somayyah <smalfraihat131@cit.just.edu.jo>
2020-04-13 06:52:12 +0000

    Finished build.sh file

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 echo "Running the build script..."
 
-HOSTNAME=$(hostname)
-echo "I am ${HOSTNAME}."
-echo "The time is $(date +%m-%d-%Y)."
-echo "The logged in user is: ${USER}."
-echo "Current working directory: ${PWD}."
+SERVER_NAME=$(hostname)
+TIMESTAMP=$(date +%m-%d-%Y)
+GIT_REPO="devops-ci-demo"
+GIT_BRANCH=$(git symbolic-ref -q HEAD)
+GIT_BRANCH=$(echo ${GIT_BRANCH##*/})
+AZURE_VARIABLE=$1
+sed -i 's/{/\${/g' 'web/index.html'
