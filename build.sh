273434ddf9f10568553febff15a3f35ec22afacb
Somayyah <smalfraihat131@cit.just.edu.jo>
2020-04-14 07:38:14 +0000

    build.sh Finalized

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ export SGIT_BRANCH=$(git symbolic-ref -q HEAD)
 export SGIT_BRANCH=$(echo ${GIT_BRANCH##*/})
 export SAZURE_VARIABLE=$1
 sed -i "s/{/\${/g" web/index.html
-envsubst '$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME $TIMESTAMP' > web/index2.html < web/index.html 
+envsubst '$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME $TIMESTAMP' > web/newIndex.html < web/index.html 
