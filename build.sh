99395f908aa0d030bb43005e2d6b4a73b5c7c50b
Somayyah <55834191+Somayyah@users.noreply.github.com>
2020-04-14 20:40:17 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ export GIT_BRANCH=$(git symbolic-ref -q HEAD)
 export GIT_BRANCH=$(echo ${GIT_BRANCH##*,})
 export AZURE_VARIABLE=$1
 sed -i "s/{/\${/g" web/index.html
-envsubst '$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME $TIMESTAMP' > web/newIndex.html < web/index.html 
+envsubst '$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME' > web/newIndex.html < web/index.html 
