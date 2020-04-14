59bbe13b8dadbc2d5fa3de3300be53a8ae007f0f
Somayyah <smalfraihat131@cit.just.edu.jo>
2020-04-14 16:55:50 +0000

    this is a commit

--- build.sh
+++ build.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-echo "Running the build script..."
-<<<<<<< HEAD
-echo "AZURE_VARIABLE=$1"
-HOSTNAME=$(hostname)
-echo "I am ${HOSTNAME}."
-echo "The time is $(date +%m-%d-%Y)."
-echo "The logged in user is: ${USER}."
-echo "Current working directory: ${PWD}."
-=======
-
 export SERVER_NAME=$(hostname)
-export STIMESTAMP=$(date +%m-%d-%Y)
-export SGIT_REPO="devops-ci-demo"
-export SGIT_BRANCH=$(git symbolic-ref -q HEAD)
-export SGIT_BRANCH=$(echo ${GIT_BRANCH##*/})
-export SAZURE_VARIABLE=$1
+export TIMESTAMP=$(date +%m-%d-%Y)
+export GIT_REPO="devops-ci-demo"
+export GIT_BRANCH=$(git symbolic-ref -q HEAD)
+export GIT_BRANCH=$(echo ${GIT_BRANCH##*/})
+export AZURE_VARIABLE=$1
 sed -i "s/{/\${/g" web/index.html
 envsubst '$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME $TIMESTAMP' > web/newIndex.html < web/index.html 
->>>>>>> test_branch
