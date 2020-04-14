bc680ca23aaa3639295c07a1feb54ddcc82d0ed9
Somayyah <55834191+Somayyah@users.noreply.github.com>
2020-04-14 20:03:19 +0300

    print get branch

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export SERVER_NAME=$(hostname)
 export TIMESTAMP=$(date +%m-%d-%Y)
 export GIT_REPO="devops-ci-demo"
 export GIT_BRANCH=$(git symbolic-ref -q HEAD)
-export GIT_BRANCH=$(echo ${GIT_BRANCH##*/})
+export GIT_BRANCH=$(echo ${GIT_BRANCH} | rev | cut -d'/' -f 1 | rev )
 export AZURE_VARIABLE=$1
 sed -i "s/{/\${/g" web/index.html
 envsubst '$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME $TIMESTAMP' > web/newIndex.html < web/index.html 
