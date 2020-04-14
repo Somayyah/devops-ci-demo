eb5a2b73b8d177468efaf70bb1f9ace74cca4071
Somayyah <smalfraihat131@cit.just.edu.jo>
2020-04-14 07:05:18 +0000

    Build.sh updated

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 echo "Running the build script..."
 
-SERVER_NAME=$(hostname)
-TIMESTAMP=$(date +%m-%d-%Y)
-GIT_REPO="devops-ci-demo"
-GIT_BRANCH=$(git symbolic-ref -q HEAD)
-GIT_BRANCH=$(echo ${GIT_BRANCH##*/})
-AZURE_VARIABLE=$1
-sed -i 's/{/\${/g' 'web/index.html'
+export SERVER_NAME=$(hostname)
+export STIMESTAMP=$(date +%m-%d-%Y)
+export SGIT_REPO="devops-ci-demo"
+export SGIT_BRANCH=$(git symbolic-ref -q HEAD)
+export SGIT_BRANCH=$(echo ${GIT_BRANCH##*/})
+export SAZURE_VARIABLE=$1
+sed -i "s/{/\${/g" web/index.html
+envsubst '$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME $TIMESTAMP' > web/index2.html < web/index.html 
