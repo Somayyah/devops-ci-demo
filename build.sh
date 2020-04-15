ad661a3e5915d71bcd83e259133f21d06ab9ae69
Somayyah <55834191+Somayyah@users.noreply.github.com>
2020-04-16 01:16:43 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@ export AZURE_VARIABLE=$1
 export GIT_REPO=$2
 export GIT_BRANCH=$3
 export SERVER_NAME=$4
+export ID=$5
+export job_status=$6
+export agent_name=$7
+export Agent_OS=$8
+export Agent_OSArchitecture=$9
 export TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 sed -i "s/{/\${/g" web/index.html
-envsubst '$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME' > web/newIndex.html <  web/index.html
+envsubst '$job_status $ID $TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME' > web/newIndex.html <  web/index.html
 cp web/newIndex.html web/index.html
