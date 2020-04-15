032fd1f9714e09f82cf04a4b9384852256318936
Somayyah <55834191+Somayyah@users.noreply.github.com>
2020-04-15 10:40:49 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-export SERVER_NAME=$(hostname)
+export AZURE_VARIABLE=$1
+export GIT_REPO=$2
+export GIT_BRANCH=$3
+export SERVER_NAME=$4
 #export TIMESTAMP=$2
 export TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
-export GIT_REPO="devops-ci-demo"
 #export GIT_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-export GIT_BRANCH=$(Build.Repository.Name)
 #export GIT_BRANCH=$(echo ${GIT_BRANCH##*,})
-export AZURE_VARIABLE=$1
 sed -i "s/{/\${/g" web/index.html
 envsubst '$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME' > web/newIndex.html < web/index.html 
