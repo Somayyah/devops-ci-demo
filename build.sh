aad770150986269dd40731a6e27f324d7b639b4d
Somayyah <55834191+Somayyah@users.noreply.github.com>
2020-04-16 01:38:20 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ export GIT_REPO=$2
 export GIT_BRANCH=$3
 export SERVER_NAME=$4
 export ID=$5
-export job_status=$6
-export agent_name=$7
-export Agent_OS=$8
-export Agent_OSArchitecture=$9
+export JOB_STATUS=$6
+export AGENT_NAME=$7
+export AGENT_OS=$8
+export AGENT_OSARCH=$9
 export TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 sed -i "s/{/\${/g" web/index.html
-envsubst '$Agent_OS $Agent_OSArchitecture $agent_name $job_status $ID $TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME' > web/newIndex.html <  web/index.html
+envsubst '$AGENT_OS $AGENT_OSARCH $AGENT_NAME $JOB_STATUS $ID $TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME' > web/newIndex.html <  web/index.html
 cp web/newIndex.html web/index.html
 echo
-echo "$Agent_OS $Agent_OSArchitecture $agent_name $job_status $ID $TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME"
+echo "$$AGENT_OS $AGENT_OSARCH $AGENT_NAME $JOB_STATUS $ID $TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME"
 echo
 cat web/newIndex.html
 echo
