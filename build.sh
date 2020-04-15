1103eddb26246626ec93c96c9466a0a3d4d35f72
Somayyah <55834191+Somayyah@users.noreply.github.com>
2020-04-15 11:15:44 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,5 @@ export GIT_BRANCH=$3
 export SERVER_NAME=$4
 export TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 sed -i "s/{/\${/g" web/index.html
-echo "$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME"
 envsubst '$TIMESTAMP $GIT_REPO $GIT_BRANCH $AZURE_VARIABLE $USER $PWD $SERVER_NAME' > web/newIndex.html <  web/index.html
-echo
-cat web/newIndex.html
-echo 
-cat web/index.html
 cp web/newIndex.html web/index.html
